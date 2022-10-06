23f7f10fa261d3e6a1c5040d9e1431204bf341f2
edamame852 <miltonycchow@gmail.com>
2022-10-06 17:33:09 +0800

    first commit

---
+++ src/main/java/tech/getarrays/employeeManger/EmployeeMangerApplication.java
@@ -0,0 +1,15 @@
+package tech.getarrays.employeeManger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Simple Spring Boot Application setup already :) 
+
+@SpringBootApplication
+public class EmployeeMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMangerApplication.class, args);
+	}
+
+}
